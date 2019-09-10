244cd7a6c9ecf6ffdffad8a8ab1b3bdde64d0e59
Stewart Parry <stusaysyo@gmail.com>
2019-09-09 23:07:59 -0400

    Added some tests for useCurrentEffect

--- src/useCurrentEffect.test.tsx
+++ src/useCurrentEffect.test.tsx
@@ -1,13 +1,133 @@
-import React from "react";
+import React, { useState } from "react";
 import { render, getByTestId } from "@testing-library/react";
+import { useCurrentEffect } from "./useCurrentEffect";
+
+jest.useFakeTimers();
+
+type Article = { name: string; content: string };
+type Api = { getArticle: (id: number) => Promise<Article> };
 
 // A testing component for us
-function TestHarness() {
-  return <div data-testid="teste">Test</div>;
-}
+const TestHarness: React.FC<{ api: Api }> = ({ api }) => {
+  const [article, setArticle] = useState(null);
+  const [articleId, setArticleId] = useState(null);
 
-it("works", () => {
-  const { container } = render(<TestHarness />);
-  const testDiv : HTMLDivElement = getByTestId(container, "teste") as HTMLDivElement;
-  expect(testDiv.textContent).toBe("Test");
+  useCurrentEffect(() => {
+    api.getArticle(articleId);
+  }, [articleId]);
+
+  return <div data-testid="teste"></div>;
+};
+describe("useCurrentEffect", () => {
+  it("Calls the effect on initial render", () => {
+    // Set up a function to check was called inside the effect
+    const testEffect = jest.fn();
+
+    // Create a test component
+    const TestHarness = () => {
+      useCurrentEffect(() => {
+        testEffect();
+      }, []);
+      return <></>;
+    };
+
+    render(<TestHarness />);
+
+    expect(testEffect).toHaveBeenCalled();
+  });
+
+  it("Calls the effect when the dependencies change", () => {
+    const testEffect = jest.fn();
+
+    const TestHarness: React.FC<{ id: number }> = ({ id }) => {
+      useCurrentEffect(() => {
+        testEffect();
+      }, [id]);
+
+      return <div>{id}</div>;
+    };
+
+    // Render 3 times with different id prop
+    const { container } = render(<TestHarness id={1} />);
+    render(<TestHarness id={2} />, { container });
+    render(<TestHarness id={3} />, { container });
+
+    expect(testEffect).toHaveBeenCalledTimes(3);
+  });
+
+  it("Does not call the effect when the dependencies don't change", () => {
+    const testEffect = jest.fn();
+
+    const TestHarness: React.FC<{ id: number }> = ({ id }) => {
+      useCurrentEffect(() => {
+        testEffect();
+      }, [id]);
+
+      return <div>{id}</div>;
+    };
+
+    // Render 2 times with the same id prop
+    const { container } = render(<TestHarness id={1} />);
+    render(<TestHarness id={1} />, { container });
+
+    expect(testEffect).toHaveBeenCalledTimes(1);
+  });
+
+  it("Calls the cleanup function when the dependencies change", () => {
+    const testCleanup = jest.fn();
+    const testEffect = jest.fn(() => testCleanup);
+
+    const TestHarness: React.FC<{ id: number }> = ({ id }) => {
+      useCurrentEffect(() => {
+        return testEffect();
+      }, [id]);
+
+      return <div>{id}</div>;
+    };
+
+    // Render 3 times with different id prop, once with the same
+    const { container } = render(<TestHarness id={1} />);
+    render(<TestHarness id={2} />, { container });
+    render(<TestHarness id={3} />, { container });
+    render(<TestHarness id={3} />, { container });
+
+    expect(testCleanup).toHaveBeenCalledTimes(2);
+  });
+
+  const delay = async (ms: number) =>
+    new Promise((resolve: TimerHandler, reject: any) => {
+      setTimeout(resolve, ms);
+    });
+
+  it("Sets isCurrent to false when the dependencies change", async () => {
+    const spy = jest.fn();
+
+    const TestHarness: React.FC<{ id: number }> = ({ id }) => {
+      useCurrentEffect(
+        effectState => {
+          setTimeout(() => {
+            if (effectState.isCurrent) {
+              spy(id);
+            }
+          }, 100);
+        },
+        [id]
+      );
+
+      return <div>{id}</div>;
+    };
+
+    // Render with initial prop
+    const { container } = render(<TestHarness id={1} />);
+    // Render again with different id prop before the timeout is resolved
+    render(<TestHarness id={2} />, { container });
+
+    // Resolve the timeout
+    jest.advanceTimersByTime(150);
+
+    // Spy should have only been called once, with second id
+    expect(spy).toHaveBeenCalledTimes(1);
+    expect(spy).toHaveBeenCalledWith(2);
+
+  });
 });
